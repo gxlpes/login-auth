6f1d990f7b6f4686ab5d66c2836e6723bbe38157
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-09-12 22:31:15 -0300

    Finished logic

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { getAuth, signOut } from "firebase/auth";
 
-const Home = () => {
-  return <div>Home Protected by Firebase</div>;
+export interface IHomePageProps {}
+
+const Home: React.FunctionComponent<IHomePageProps> = (props) => {
+  const auth = getAuth();
+
+  return (
+    <>
+      <div>Home Protected by Firebase</div>
+      <button onClick={() => signOut(auth)}>Sign out of Firebase</button>
+    </>
+  );
 };
 
 export default Home;
