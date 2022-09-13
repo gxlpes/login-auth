93f415f903be375fa3a292275a82e24b1dfb43af
Guilherme Lopes <guilhermxlopes@gmail.com>
2022-09-12 22:04:17 -0300

    Initial files

---
+++ src/pages/Login.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export interface ILoginPageProps {}
+
+const Login: React.FunctionComponent<ILoginPageProps> = (props) => {
+  return <div>Login</div>;
+};
+
+export default Login;
